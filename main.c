a8de75fdce4fa2318e5f07da03a0878687002a7e
Akira Sato <amvvec@gmail.com>
2025-10-05 16:30:08 +0300

    fix bug with .exit command

--- main.c
+++ main.c
@@ -280,7 +280,7 @@ int main(int argc, char** argv)
     {
         print_prompt();
         read_input(input_buffer);
-        if(input_buffer->buffer[0] == ".")
+        if(input_buffer->buffer[0] == '.')
         {
             switch(do_meta_command(input_buffer, table))
             {

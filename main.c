48f031488e9b311645763a2eb8307a339003d71f
Akira Sato <amvvec@gmail.com>
2025-10-05 16:04:35 +0300

    add PREPARE_STRING_TOO_LONG macro add prepare_insert function with safety

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ typedef enum
 {
     PREPARE_SUCCESS,
     PREPARE_SYNTAX_ERROR,
+    PREPARE_STRING_TOO_LONG,
     PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
@@ -36,8 +37,8 @@ typedef enum
 typedef struct
 {
     ssize_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USERNAME_SIZE + 1];
+    char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 
 typedef struct
@@ -152,6 +153,39 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
         return PREPARE_SUCCESS;
     }
     return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+PrepareResult prepare_insert(InputBuffer * input_buffer, Statement * statement)
+{
+    statement->type = STATEMENT_INSERT;
+
+    char * keyword = strtok(input_buffer->buffer, " ");
+    char * id_string = strtok(NULL, " ");
+    char * username = strtok(NULL, " ");
+    char * email = strtok(NULL, " ");
+
+    if(id_string == NULL || username == NULL || email == NULL)
+    {
+        return PREPARE_SYNTAX_ERROR;
+    }
+
+    int id = atoi(id_string);
+
+    if(strlen(username) > COLUMN_USERNAME_SIZE)
+    {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if(strlen(email) > COLUMN_EMAIL_SIZE)
+    {
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement->row_to_insert.id = id;
+
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
 }
 
 const int ID_OFFSET = 0;

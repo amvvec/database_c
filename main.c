d416cd43902d1d37aab0b6799828e78f347564b0
Akira Sato <amvvec@gmail.com>
2025-10-16 20:27:51 +0300

    update execute_insert(). function always chooses to insert at the end of the table

--- main.c
+++ main.c
@@ -626,14 +626,24 @@ void print_row(Row* row)
 ExecuteResult execute_insert(Statement* statement, Table* table)
 {
     void* node = get_page(table->pager, table->root_page_num);
-    if((*leaf_node_num_cells(node) >= LEAF_NODE_MAX_CELLS))
+    int num_cells = (*leaf_node_num_cells(node));
+    if((num_cells >= LEAF_NODE_MAX_CELLS))
     {
         return EXECUTE_TABLE_FULL;
     }
 
     Row* row_to_insert = &(statement->row_to_insert);
-    Cursor* cursor = table_end(table);
-    leaf_node_insert(cursor, row_to_insert->id, row_to_insert);
+    int key_to_insert = row_to_insert->id;
+    Cursor* cursor = table_find(table, key_to_insert);
+
+    if(cursor->cell_num < num_cells)
+    {
+        int key_at_index = *leaf_node_key(node, cursor->cell_num);
+        if(key_at_index == key_to_insert)
+        {
+            return EXECUTE_DUPLICATE_KEY;
+        }
+    }
 
     free(cursor);
 

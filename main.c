873f5d9c372eb69b1bf7f559bf8472555946cb25
Akira Sato <amvvec@gmail.com>
2025-10-25 22:12:11 +0300

    feat(leaf_node_split_and_insert): Copy every cell into its new location

--- main.c
+++ main.c
@@ -231,6 +231,10 @@ const int LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
 const int LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
 const int LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
 
+const int LEAF_NODE_RIGHT_SPLIT_COUNT = (LEAF_NODE_MAX_CELLS + 1) / 2;
+const int LEAF_NODE_LEFT_SPLIT_COUNT =
+    (LEAF_NODE_MAX_CELLS + 1) - LEAF_NODE_RIGHT_SPLIT_COUNT;
+
 void* leaf_node_cell(void* node, int cell_num)
 {
     return node + LEAF_NODE_HEADER_SIZE + cell_num + LEAF_NODE_CELL_SIZE;
@@ -623,23 +627,56 @@ void print_row(Row* row)
     printf("(%d, %s, %s)\n", row->id, row->username, row->email);
 }
 
-int get_unused_page_num(Pager * pager)
+int get_unused_page_num(Pager* pager)
 {
     return pager->num_pages;
 }
 
-
-void leaf_node_split_and_insert(Cursor * cursor, int key, Row * row)
+void leaf_node_split_and_insert(Cursor* cursor, int key, Row* value)
 {
     /**
      * create a new node and move half the cells over
      * insert the new value in one of the two nodes
      * update parent or create a new parent
      */
-    void * old_node = get_page(cursor->table->pager, cursor->page_num);
     int new_num_page = get_unused_page_num(cursor->table->pager);
-    void * new_node = get_page(cursor->table->pager, new_num_page);
+
+    void* new_node = get_page(cursor->table->pager, new_num_page);
+    void* old_node = get_page(cursor->table->pager, cursor->page_num);
+
     initialize_leaf_node(new_node);
+
+    for(int i = LEAF_NODE_MAX_CELLS; i >= 0; i--)
+    {
+        void* destination_node;
+
+        if(i >= LEAF_NODE_LEFT_SPLIT_COUNT)
+        {
+            destination_node = new_node;
+        }
+        else
+        {
+            destination_node = old_node;
+        }
+
+        int index_within_node = i % LEAF_NODE_LEFT_SPLIT_COUNT;
+        void* destination = leaf_node_cell(destination_node, index_within_node);
+
+        if(i == cursor->cell_num)
+        {
+            serialize_row(value, destination);
+        }
+        else if(i > cursor->cell_num)
+        {
+            memcpy(destination, leaf_node_cell(old_node, i - 1),
+                   LEAF_NODE_CELL_SIZE);
+        }
+        else
+        {
+            memcpy(destination, leaf_node_cell(old_node, i),
+                   LEAF_NODE_CELL_SIZE);
+        }
+    }
 }
 
 ExecuteResult execute_insert(Statement* statement, Table* table)
